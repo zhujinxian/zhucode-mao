9acaae4eb9a3e1d3aafc7d6cd60ef207188c7ea1
zhu <zhu@zhu-Inspiron-3443.(none)>
2015-12-17 14:45:09 +0800

    add multi dbs in one dbsource feature

--- src/main/java/com/zhucode/mao/mongo/dataaccess/DefaultDataSourceFactory.java
+++ src/main/java/com/zhucode/mao/mongo/dataaccess/DefaultDataSourceFactory.java
@@ -17,21 +17,41 @@ public class DefaultDataSourceFactory implements DataSourceFactory {
 	private MongoClient mongo;
 	
 	private Map<String, DB> dbs = new HashMap<String, DB>();
+
+	private Map<String, String> dbmap;
 	
-	public DefaultDataSourceFactory(MongoClientOptions mc, String host) {
+	public DefaultDataSourceFactory(MongoClientOptions mc, String host, Map<String, String> dbmap) {
 		mongo = new MongoClient(host, mc);
+		this.dbmap = dbmap;
 	}
 	
 	
 	@SuppressWarnings("deprecation")
 	@Override
 	public DB getDataSource(String db) {
+		if (db.startsWith("$")) {
+			String[] strs = db.replace("$", "").replace("{", "").replace("}", "").split(":");
+			String dbKey = strs[0].replaceAll(" ", "");
+			String dbVal = null;
+			if (strs.length == 2) {
+				dbVal = strs[1].replaceAll(" ", "");
+			}
+			if (dbmap != null && dbmap.containsKey(dbKey)) {
+				dbVal = dbmap.get(dbKey);
+			}
+			if (dbVal != null) {
+				db = dbVal;
+			}
+		}
 		if (dbs.containsKey(db)) {
 			return dbs.get(db);
 		}
 		synchronized (this) {
 			DB d = mongo.getDB(db);
 			dbs.put(db, d);
+			String hp = mongo.getAddress().getHost() 
+					+ ":" + mongo.getAddress().getPort();
+			System.out.println("connect to mongo [" + hp + "][" + db + "]");
 			return d;
 		}
 		
